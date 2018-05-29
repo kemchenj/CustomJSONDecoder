65fe662bb691c7dc02ded0c8a2e34d7a0056f8c3
kemchenj <chenkem95@outlook.com>
2018-05-29 22:07:24 +0800

    fix: 修复原生 Decoder 的 superDecoder 方法错误问题

--- JSONDecoderTests/JSONCoderTests.swift
+++ JSONDecoderTests/JSONCoderTests.swift
@@ -12,16 +12,101 @@ import JSONDecoder
 
 class JSONCoderTests: XCTestCase {
 
-    func getData() -> Data {
+    func largeData() -> Data {
         let parent = (#file).components(separatedBy: "/").dropLast().joined(separator: "/")
         let url = URL(string: "file://\(parent)/large.json")!
         print("Loading fixture from url \(url)")
 
         return try! Data(contentsOf: url)
     }
+
+    func testData() -> Data {
+        return """
+        {
+          "super" : {
+            "_id": "56c658d3e425523f3a636e64",
+            "index": 0,
+            "guid": "5a651472-d37b-4ad7-a556-9de70aa0dc28",
+            "isActive": true,
+            "balance": "$2,906.67",
+            "picture": "http://placehold.it/32x32",
+            "age": 37,
+            "name": "Park Odom",
+            "company": "SENSATE",
+            "email": "parkodom@sensate.com",
+            "phone": "+1 (954) 436-2958",
+            "address": "406 Forest Place, Brownlee, American Samoa, 3358",
+            "about": "Nisi enim amet proident ut labore voluptate cillum ea exercitation mollit reprehenderit occaecat labore. Nulla excepteur consectetur qui magna. Minim esse labore consequat sit dolore minim. Dolor eiusmod non laboris sit magna eu. Qui deserunt Lorem pariatur magna deserunt ad sit nostrud eu occaecat.",
+            "registered": "2014-05-16T11:36:46 -01:00",
+            "latitude": -25.472313,
+            "longitude": 122.375678,
+            "tags": [
+              "nisi",
+              "minim",
+              "dolor",
+              "in",
+              "nisi",
+              "esse",
+              "magna"
+            ],
+            "greeting": "Hello, Park Odom! You have 2 unread messages.",
+            "favoriteFruit": "banana"
+          },
+          "_id": "56c658d3e425523f3a636e64",
+          "index": 0,
+          "guid": "5a651472-d37b-4ad7-a556-9de70aa0dc28",
+          "isActive": true,
+          "balance": "$2,906.67",
+          "picture": "http://placehold.it/32x32",
+          "age": 37,
+          "name": "Park Odom",
+          "company": "SENSATE",
+          "email": "parkodom@sensate.com",
+          "phone": "+1 (954) 436-2958",
+          "address": "406 Forest Place, Brownlee, American Samoa, 3358",
+          "about": "Nisi enim amet proident ut labore voluptate cillum ea exercitation mollit reprehenderit occaecat labore. Nulla excepteur consectetur qui magna. Minim esse labore consequat sit dolore minim. Dolor eiusmod non laboris sit magna eu. Qui deserunt Lorem pariatur magna deserunt ad sit nostrud eu occaecat.",
+          "registered": "2014-05-16T11:36:46 -01:00",
+          "latitude": -25.472313,
+          "longitude": 122.375678,
+          "tags": [
+            "nisi",
+            "minim",
+            "dolor",
+            "in",
+            "nisi",
+            "esse",
+            "magna"
+          ],
+          "greeting": "Hello, Park Odom! You have 2 unread messages.",
+          "favoriteFruit": "banana",
+          "eyeColor": "blue",
+          "gender": "male",
+          "friends": [
+            {
+              "id": 0,
+              "name": "Ortiz Garrison"
+            }
+          ]
+        }
+        """.data(using: .utf8)!
+    }
+
+    func testCustomDecoder() {
+        let data = testData()
+
+        do {
+            let _ = try! JSONSerialization.jsonObject(with: data, options: [])
+            let originDecoderResult = try! JSONDecoder().decode(User.self, from: data)
+            let customDecoderResult = try CustomDecoder.decode(User.self, from: data)
+
+            XCTAssertEqual(customDecoderResult, originDecoderResult)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
     
     func testPerformanceCustomDecoder() {
-        let data = getData()
+        let data = largeData()
 
         measure {
             _ = try! CustomDecoder.decode([User].self, from: data)
@@ -29,10 +114,10 @@ class JSONCoderTests: XCTestCase {
     }
 
     func testPerformanceOriginalDecoder() {
-        let data = getData()
+        let data = largeData()
 
         measure {
-            _ = try! JSONDecoder().decode([User].self, from: data)
+            _ = try! JSONDecoder().decode([TUser].self, from: data)
         }
     }
 
--- JSONDecoderTests/UserModel.swift
+++ JSONDecoderTests/UserModel.swift
@@ -4,86 +4,184 @@ import JSONDecoder
 
 // The model that the JSONObject in large.json in Fixtures models.
 
-public struct User: Codable {
-  public let _id: String
-  public let index: Int
-  public let guid: String
-  public let isActive: Bool
-  public let balance: String
-  public let picture: String
-  public let age: Int
-  public let eyeColor: Color
-  public let name: String
-  public let gender: Gender
-  public let company: String
-  public let email: String
-  public let phone: String
-  public let address: String
-  public let about: String
-  public let registered: String
-  public let latitude: Double
-  public let longitude: Double
-  public let tags: [String]
-  public let friends: [Friend]
-  public let greeting: String
-  public let favoriteFruit: String
+class BaseClass: Codable {
 
-  public enum Color: String, Codable {
-    case red
-    case green
-    case blue
-    case brown
-  }
+    let _id: String
+    let index: Int
+    let guid: String
+    let isActive: Bool
+    let balance: String
+    let picture: String
+    let age: UInt
+    let name: String
+    let company: String
+    let email: String
+    let phone: String
+    let address: String
+    let about: String
+    let registered: String
+    let latitude: Double
+    let longitude: Double
+    let tags: [String]
+    let greeting: String
+    let favoriteFruit: String
 
-  public enum Gender: String, Codable {
-    case male
-    case female
-  }
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
 
-  public struct Friend: Codable {
-    public let id: Int
-    public let name: String
-  }
+        _id = try container.decode(String.self, forKey: ._id)
+        index = try container.decode(Int.self, forKey: .index)
+        guid = try container.decode(String.self, forKey: .guid)
+        isActive = try container.decode(Bool.self, forKey: .isActive)
+        balance = try container.decode(String.self, forKey: .balance)
+        picture = try container.decode(String.self, forKey: .picture)
+        age = try container.decode(UInt.self, forKey: .age)
+        name = try container.decode(String.self, forKey: .name)
+        company = try container.decode(String.self, forKey: .company)
+        email = try container.decode(String.self, forKey: .email)
+        phone = try container.decode(String.self, forKey: .phone)
+        address = try container.decode(String.self, forKey: .address)
+        about = try container.decode(String.self, forKey: .about)
+        registered = try container.decode(String.self, forKey: .registered)
+        latitude = try container.decode(Double.self, forKey: .latitude)
+        longitude = try container.decode(Double.self, forKey: .longitude)
+        tags = try container.decode([String].self, forKey: .tags)
+        greeting = try container.decode(String.self, forKey: .greeting)
+        favoriteFruit = try container.decode(String.self, forKey: .favoriteFruit)
+    }
+
+    init(foundationJSON json: Any) throws {
+        guard
+            let json              = json as? [String: Any],
+            let id                = json["_id"] as? String,
+            let index             = json["index"] as? Int,
+            let guid              = json["guid"] as? String,
+            let isActive          = json["isActive"] as? Bool,
+            let balance           = json["balance"] as? String,
+            let picture           = json["picture"] as? String,
+            let age               = json["age"] as? UInt,
+            let name              = json["name"] as? String,
+            let company           = json["company"] as? String,
+            let email             = json["email"] as? String,
+            let phone             = json["phone"] as? String,
+            let address           = json["address"] as? String,
+            let about             = json["about"] as? String,
+            let registered        = json["registered"] as? String,
+            let latitude          = json["latitude"] as? Double,
+            let longitude         = json["longitude"] as? Double,
+            let tags              = json["tags"] as? [String],
+            let greeting          = json["greeting"] as? String,
+            let favoriteFruit     = json["favoriteFruit"] as? String
+        else { throw FoundationJSONError.typeMismatch }
+
+        self._id             = id
+        self.index          = index
+        self.guid           = guid
+        self.isActive       = isActive
+        self.balance        = balance
+        self.picture        = picture
+        self.age            = age
+        self.name           = name
+        self.company        = company
+        self.email          = email
+        self.phone          = phone
+        self.address        = address
+        self.about          = about
+        self.registered     = registered
+        self.latitude       = latitude
+        self.longitude      = longitude
+        self.tags           = tags
+        self.greeting       = greeting
+        self.favoriteFruit  = favoriteFruit
+    }
 }
 
-// MARK: - vdka/json
+class User: BaseClass, Equatable {
+    let eyeColor: Color
+    let gender: Gender
+    let friends: [Friend]
 
-//extension User.Friend: JSONInitializable {
-//
-//  public init(json: JSONObject) throws {
-//    self.id   = try json.get("id")
-//    self.name = try json.get("name")
-//  }
-//}
-//
-//extension User: JSONInitializable {
-//
-//  public init(json: JSONObject) throws {
-//    self._id            = try json.get("_id")
-//    self.index          = try json.get("index")
-//    self.guid           = try json.get("guid")
-//    self.isActive       = try json.get("isActive")
-//    self.balance        = try json.get("balance")
-//    self.picture        = try json.get("picture")
-//    self.age            = try json.get("age")
-//    self.eyeColor       = try json.get("eyeColor")
-//    self.name           = try json.get("name")
-//    self.gender         = try json.get("gender")
-//    self.company        = try json.get("company")
-//    self.email          = try json.get("email")
-//    self.phone          = try json.get("phone")
-//    self.address        = try json.get("address")
-//    self.about          = try json.get("about")
-//    self.registered     = try json.get("registered")
-//    self.latitude       = try json.get("latitude")
-//    self.longitude      = try json.get("longitude")
-//    self.tags           = try json.get("tags")
-//    self.friends        = try json.get("friends")
-//    self.greeting       = try json.get("greeting")
-//    self.favoriteFruit  = try json.get("favoriteFruit")
-//  }
-//}
+    static func ==(lhs: User, rhs: User) -> Bool {
+        return lhs._id == rhs._id
+            && lhs.index == rhs.index
+            && lhs.guid == rhs.guid
+            && lhs.isActive == rhs.isActive
+            && lhs.balance == rhs.balance
+            && lhs.picture == rhs.picture
+            && lhs.age == rhs.age
+            && lhs.name == rhs.name
+            && lhs.company == rhs.company
+            && lhs.email == rhs.email
+            && lhs.phone == rhs.phone
+            && lhs.address == rhs.address
+            && lhs.about == rhs.about
+            && lhs.registered == rhs.registered
+            && lhs.latitude == rhs.latitude
+            && lhs.longitude == rhs.longitude
+            && lhs.tags == rhs.tags
+            && lhs.greeting == rhs.greeting
+            && lhs.favoriteFruit == rhs.favoriteFruit
+            && lhs.eyeColor == rhs.eyeColor
+            && lhs.gender == rhs.gender
+            && lhs.friends == rhs.friends
+    }
 
+    enum Color: String, Codable {
+        case red
+        case green
+        case blue
+        case brown
+    }
+
+    enum Gender: String, Codable {
+        case male
+        case female
+    }
+
+    struct Friend: Codable, Equatable {
+        let id: Int
+        let name: String
+
+        static func ==(lhs: Friend, rhs: Friend) -> Bool {
+            return lhs.id == rhs.id
+                && lhs.name == rhs.name
+        }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case eyeColor
+        case gender
+        case friends
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        eyeColor = try container.decode(Color.self, forKey: .eyeColor)
+        gender   = try container.decode(Gender.self, forKey: .gender)
+        friends  = try container.decode([Friend].self, forKey: .friends)
+
+        // try super.init(from: container.superDecoder())
+        try super.init(from: container.superDecoder())
+    }
+
+    override init(foundationJSON json: Any) throws {
+        guard
+            let json              = json as? [String: Any],
+            let eyeColorRawValue  = json["eyeColor"] as? String,
+            let eyeColor          = Color(rawValue: eyeColorRawValue),
+            let genderRawValue    = json["gender"] as? String,
+            let gender            = Gender(rawValue: genderRawValue),
+            let friendsObjects    = json["friends"] as? [Any]
+        else { throw FoundationJSONError.typeMismatch }
+
+        self.friends  = try friendsObjects.map(Friend.init)
+        self.eyeColor = eyeColor
+        self.gender   = gender
+
+        try super.init(foundationJSON: json)
+    }
+}
 
 // MARK: - Foundation
 
@@ -93,7 +191,7 @@ enum FoundationJSONError: Error {
 
 extension User.Friend {
 
-  public init(foundationJSON json: Any) throws {
+  init(foundationJSON json: Any) throws {
     guard
       let json  = json as? [String: Any],
       let id    = json["id"] as? Int,
@@ -104,60 +202,99 @@ extension User.Friend {
   }
 }
 
-extension User {
+class TUser: BaseClass, Equatable {
+    let eyeColor: Color
+    let gender: Gender
+    let friends: [Friend]
 
-  public init(foundationJSON json: Any) throws {
-    guard
-      let json              = json as? [String: Any],
-      let id                = json["_id"] as? String,
-      let index             = json["index"] as? Int,
-      let guid              = json["guid"] as? String,
-      let isActive          = json["isActive"] as? Bool,
-      let balance           = json["balance"] as? String,
-      let picture           = json["picture"] as? String,
-      let age               = json["age"] as? Int,
-      let eyeColorRawValue  = json["eyeColor"] as? String,
-      let eyeColor          = Color(rawValue: eyeColorRawValue),
-      let name              = json["name"] as? String,
-      let genderRawValue    = json["gender"] as? String,
-      let gender            = Gender(rawValue: genderRawValue),
-      let company           = json["company"] as? String,
-      let email             = json["email"] as? String,
-      let phone             = json["phone"] as? String,
-      let address           = json["address"] as? String,
-      let about             = json["about"] as? String,
-      let registered        = json["registered"] as? String,
-      let latitude          = json["latitude"] as? Double,
-      let longitude         = json["longitude"] as? Double,
-      let tags              = json["tags"] as? [String],
-      let friendsObjects    = json["friends"] as? [Any],
-      let greeting          = json["greeting"] as? String,
-      let favoriteFruit     = json["favoriteFruit"] as? String
-      else { throw FoundationJSONError.typeMismatch }
+    static func ==(lhs: TUser, rhs: TUser) -> Bool {
+        return lhs._id == rhs._id
+            && lhs.index == rhs.index
+            && lhs.guid == rhs.guid
+            && lhs.isActive == rhs.isActive
+            && lhs.balance == rhs.balance
+            && lhs.picture == rhs.picture
+            && lhs.age == rhs.age
+            && lhs.name == rhs.name
+            && lhs.company == rhs.company
+            && lhs.email == rhs.email
+            && lhs.phone == rhs.phone
+            && lhs.address == rhs.address
+            && lhs.about == rhs.about
+            && lhs.registered == rhs.registered
+            && lhs.latitude == rhs.latitude
+            && lhs.longitude == rhs.longitude
+            && lhs.tags == rhs.tags
+            && lhs.greeting == rhs.greeting
+            && lhs.favoriteFruit == rhs.favoriteFruit
+            && lhs.eyeColor == rhs.eyeColor
+            && lhs.gender == rhs.gender
+            && lhs.friends == rhs.friends
+    }
 
-    self._id             = id
-    self.index          = index
-    self.guid           = guid
-    self.isActive       = isActive
-    self.balance        = balance
-    self.picture        = picture
-    self.age            = age
-    self.eyeColor       = eyeColor
-    self.name           = name
-    self.gender         = gender
-    self.company        = company
-    self.email          = email
-    self.phone          = phone
-    self.address        = address
-    self.about          = about
-    self.registered     = registered
-    self.latitude       = latitude
-    self.longitude      = longitude
-    self.tags           = tags
-    self.friends        = try friendsObjects.map(Friend.init)
-    self.greeting       = greeting
-    self.favoriteFruit  = favoriteFruit
-  }
+    enum Color: String, Codable {
+        case red
+        case green
+        case blue
+        case brown
+    }
+
+    enum Gender: String, Codable {
+        case male
+        case female
+    }
+
+    struct Friend: Codable, Equatable {
+        let id: Int
+        let name: String
+
+        static func ==(lhs: Friend, rhs: Friend) -> Bool {
+            return lhs.id == rhs.id
+                && lhs.name == rhs.name
+        }
+
+        init(foundationJSON json: Any) throws {
+            guard
+                let json  = json as? [String: Any],
+                let id    = json["id"] as? Int,
+                let name  = json["name"] as? String
+            else { throw FoundationJSONError.typeMismatch }
+            self.id   = id
+            self.name = name
+        }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case eyeColor
+        case gender
+        case friends
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        eyeColor = try container.decode(Color.self, forKey: .eyeColor)
+        gender   = try container.decode(Gender.self, forKey: .gender)
+        friends  = try container.decode([Friend].self, forKey: .friends)
+
+        // try super.init(from: container.superDecoder())
+        try super.init(from: decoder)
+    }
+
+    override init(foundationJSON json: Any) throws {
+        guard
+            let json              = json as? [String: Any],
+            let eyeColorRawValue  = json["eyeColor"] as? String,
+            let eyeColor          = Color(rawValue: eyeColorRawValue),
+            let genderRawValue    = json["gender"] as? String,
+            let gender            = Gender(rawValue: genderRawValue),
+            let friendsObjects    = json["friends"] as? [Any]
+            else { throw FoundationJSONError.typeMismatch }
+
+        self.friends  = try friendsObjects.map(Friend.init)
+        self.eyeColor = eyeColor
+        self.gender   = gender
+
+        try super.init(foundationJSON: json)
+    }
 }
-
-
